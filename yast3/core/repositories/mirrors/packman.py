4b0046e82b149c87a7337af98080b12a73833778
Guo Yunhe <i@guoyunhe.me>
2026-07-01 10:36:50 +0800

    refactor(repomirrors): update packman and opensuse mirror lists
    1. 优化packman镜像源：移除阿里云镜像的http协议、简化阿里云组织名称，更新卡尔斯巴德镜像组织名称，修正亚琛工业大学镜像域名，移除部分冗余协议
    2. 大幅扩充opensuse镜像源：新增多个国内高校镜像、新增更多地区镜像源，修正部分镜像路径和协议配置，更新部分镜像的组织信息

--- yast3/core/repositories/mirrors/packman.py
+++ yast3/core/repositories/mirrors/packman.py
@@ -1,9 +1,9 @@
 from yast3.core.repositories.mirrors.mirror import Mirror
 
 packman_mirrors = [
-    Mirror(url='mirrors.aliyun.com/packman/', organization='Alibaba Cloud (阿里云)', location='China', sync_frequency='24h', protocols=['http', 'https']),
-    Mirror(url='mirror.karneval.cz/pub/linux/packman/', organization='Karneval (Vodafone)', location='Czech', sync_frequency='1h', protocols=['http', 'https', 'rsync']),
-    Mirror(url='ftp.fau.de/packman/', organization='Friedrich-Alexander University', location='Germany', sync_frequency='1h', protocols=['http', 'https', 'ftp']),
-    Mirror(url='ftp.hal-aachen.de/p-aachen.de/packman/', organization='RWTH Aachen University', location='Germany', sync_frequency='1h', protocols=['http', 'https', 'ftp', 'rsync']),
-    Mirror(url='ftp.gwdg.de/pub/linux/misc/packman/', organization='GWDG', location='Germany', sync_frequency='4h', protocols=['http', 'https', 'ftp'])
+    Mirror(url='mirrors.aliyun.com/packman/', organization='Alibaba Cloud', location='China', sync_frequency='24h', protocols=['https']),
+    Mirror(url='mirror.karneval.cz/pub/linux/packman/', organization='Vodafone Czech', location='Czech', sync_frequency='1h', protocols=['http', 'rsync']),
+    Mirror(url='ftp.fau.de/packman/', organization='Friedrich-Alexander University', location='Germany', sync_frequency='1h', protocols=['http', 'ftp']),
+    Mirror(url='ftp.halifax.rwth-aachen.de/packman/', organization='RWTH Aachen University', location='Germany', sync_frequency='1h', protocols=['http', 'ftp', 'rsync']),
+    Mirror(url='ftp.gwdg.de/pub/linux/misc/packman/', organization='GWDG', location='Germany', sync_frequency='4h', protocols=['http', 'ftp']),
 ]
