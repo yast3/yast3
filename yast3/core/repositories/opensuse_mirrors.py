4789547bfb1b1760052a0c4c7e3796a41944eeb9
Guo Yunhe <i@guoyunhe.me>
2026-07-04 15:34:21 +0800

    feat(core/repositories): rename mirror

--- yast3/core/repositories/opensuse_mirrors.py
+++ yast3/core/repositories/opensuse_mirrors.py
@@ -36,7 +36,7 @@ opensuse_mirrors = [
     Mirror(url='ftp.linux.cz/pub/linux/opensuse/', organization='CZLUG', location='Czech', sync_frequency='1h', protocols=['http', 'https', 'ftp', 'rsync']),
     Mirror(url='ftp.sh.cvut.cz/opensuse/', organization='Silicon Hill CTU', location='Czech', sync_frequency='1h', protocols=['http', 'https', 'ftp', 'rsync']),
     Mirror(url='mirror.karneval.cz/pub/linux/opensuse/', organization='Vodafone Czech', location='Czech', sync_frequency='1h', protocols=['https', 'ftp', 'rsync']),
-    Mirror(url='ftp.fau.de/opensuse/', organization='Friedrich-Alexander University', location='Germany', sync_frequency='1h', protocols=['http', 'https', 'ftp']),
+    Mirror(url='ftp.fau.de/opensuse/', organization='University of Erlangen–Nuremberg (FAU)', location='Germany', sync_frequency='1h', protocols=['http', 'https', 'ftp']),
     Mirror(url='ftp.halifax.rwth-aachen.de/opensuse/', organization='RWTH Aachen University', location='Germany', sync_frequency='1h', protocols=['http', 'https', 'ftp', 'rsync']),
     Mirror(url='ftp.gwdg.de/pub/opensuse/', organization='GWDG', location='Germany', sync_frequency='4h', protocols=['http', 'https', 'ftp', 'rsync']),
     Mirror(url='ftp.rz.uni-wuerzburg.de/linux/opensuse/', organization='University of Wuerzburg', location='Germany', sync_frequency='1h', protocols=['http', 'https', 'ftp', 'rsync']),
